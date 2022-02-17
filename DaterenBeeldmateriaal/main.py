455a5ffaeaf573ff89c568b4f7bae40fb2a226f8
decoseti <Tine.Decosemaker@stad.gent>
2022-02-17 17:26:54 +0100

    Spelen met Lay-out: tekstblokken langs elkaar proberen te krijgen (mislukt)

--- DaterenBeeldmateriaal/main.py
+++ DaterenBeeldmateriaal/main.py
@@ -1,65 +1,64 @@
-from ttkwidgets.autocomplete import AutocompleteCombobox
+from ttkwidgets.autocomplete import AutocompleteEntryListbox
 from tkinter import *
 import tkinter as tk
 
 interface = tk.Tk()
 interface.configure(bg="#60c1c9")
-interface.geometry("1200x700+30+30")
+interface.geometry('1000x2000')
 
 #kleurcode lichtblauw = #60c1c9
 #kleurcode geel = #fdc20b
 #kleurcode roze = #ea94aa
 #kleurcode groen = #00a57
 
-titel = tk.Label(interface, text="DATEREN VAN BEELDMATERIAAL")
-titel.configure(bg="#60c1c9", fg="#000000", font=("Calibri",20,"bold"), padx=500, pady=30)
+titel = Label(interface, text="DATEREN VAN BEELDMATERIAAL")
+titel.configure(bg="#60c1c9", fg="#000000", font=("Calibri",20,"bold"))
+titel.grid(row=1, column=0)
 
 #OPTIE 1: op basis van foto-techniek
 #OPTIE 2: op basis van bezienswaardigheden
     #doorzoek thesaurus op vaste woorden
 
-lijst = ['Tine', 'Saar', 'Flore']
+lijst = ['Tine', 'Saar', 'Flore', 'Sarrah', 'a', 'b', 'c', 'd', 'e','f', 'g', 'h']
+#zorgen dat enkel de zaken nog zichtbaar zijn zodra je een letter intypt (option list dus)
 
-frame = Frame(interface, bg='#f25252')
-frame.pack(expand=True)
+frame = Frame(interface, bg="#60c1c9")
+frame.grid()
 
 Label(
     frame,
     text = "Duid hier aan wat te zien is op de foto. Zoektermen moeten in het Nederlands en enkelvoud genoteerd worden.",
     bg="#60c1c9",
     fg="#000000",
-    padx=5,
-    pady=15,
     font = ("Calibri",12)
-).pack()
-#frame.place(x=30, y=30, width=1000, height=20)
-#https://www.activestate.com/resources/quick-reads/how-to-position-widgets-in-tkinter/ --> posities aanpassen. Kunnen we van pack terug naar grid gaan?
+).grid(row=1, column=0, padx=3, pady=3)
 
-entry = AutocompleteCombobox(
+entry_1 = AutocompleteEntryListbox(
     frame,
-    width=30,
-    font=('Times, 18'),
+    width=20,
+    height=10,
+    font=('Times, 8'),
     completevalues=lijst
 )
-entry.pack()
-#entry.place(x=80, y=130, width=300, height=50)
+entry_1.grid(row=2, column=0, padx=20, pady=1)
 
-entry_2 = AutocompleteCombobox(
+entry_2 = AutocompleteEntryListbox(
     frame,
-    width=30,
+    width=20,
+    height=10,
     font=('Times, 18'),
     completevalues=lijst,
     )
-entry_2.pack()
+entry_2.grid(row=2, column=0, ipadx=10, ipady=10)
 
-entry_3 = AutocompleteCombobox(
+entry_3 = AutocompleteEntryListbox(
     frame,
-    width=30,
-    font=('Times, 18'),
+    width=20,
+    height=10,
+    font=('Times, 12'),
     completevalues=lijst
 )
-entry_3.pack()
-
+entry_3.grid(row=2, column=0, padx=200, pady=100)
 
 
 #OPTIE 3: op basis van fotograaf
