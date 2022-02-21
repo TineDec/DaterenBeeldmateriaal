1386b923c7c54d9afd626442cef06edef86734b4
decoseti <Tine.Decosemaker@stad.gent>
2022-02-21 16:48:52 +0100

    Optionlist compleet! Nog verdubbelen en lay-out

--- DaterenBeeldmateriaal/main.py
+++ DaterenBeeldmateriaal/main.py
@@ -1,3 +1,5 @@
+from tkinter import Listbox
+
 from ttkwidgets.autocomplete import AutocompleteEntryListbox
 from tkinter import *
 import tkinter as tk
@@ -5,6 +7,38 @@ import tkinter as tk
 interface = tk.Tk()
 interface.configure(bg="#60c1c9")
 interface.geometry('1000x2000')
+
+def update(data):
+    my_list.delete(0, END)
+
+    for item in data:
+        my_list.insert(END, item)
+
+def check(e):
+    typed = entry_1.get()
+
+    if typed == '':
+        data = lijst
+    else:
+        data = []
+        for item in lijst:
+            if typed.lower() in item.lower():
+                data.append(item)
+    update(data)
+
+def fillout(e):
+    entry_1.delete(0, END)
+    entry_1.insert(0, my_list.get(ACTIVE))
+
+
+entry_1 = Entry(interface)
+entry_1.grid(row=1, column=3)
+entry_1.grid()
+my_list: Listbox = Listbox(interface)
+my_list.grid(row=2, column=3)
+my_list.grid()
+
+my_list.bind("<<ListboxSelect>>", fillout)
 
 #kleurcode lichtblauw = #60c1c9
 #kleurcode geel = #fdc20b
@@ -20,45 +54,37 @@ titel.grid(row=1, column=0, columnspan=2)
     #doorzoek thesaurus op vaste woorden
 
 lijst = ['Tine', 'Saar', 'Flore', 'Sarrah', 'tram met elect. bovenleidingen', 'paardentram', 'tram met paard', 'tram zonder elec. bovenleiding', 'e','f', 'g', 'h']
-#zorgen dat enkel de zaken nog zichtbaar zijn zodra je een letter intypt (option list dus)
 
-frame = Frame(interface, bg="#60c1c9")
-frame.grid()
+#frame = Entry(interface, bg="#60c1c9")
+#frame.grid()
 
 Label(
-    frame,
     text = "Duid hier aan wat te zien is op de foto. Zoektermen moeten in het Nederlands en enkelvoud genoteerd worden.",
     bg="#60c1c9",
     fg="#000000",
     font = ("Calibri",12)
 ).grid(row=1, column=1, columnspan=2, pady=20)
 
-entry_1 = AutocompleteEntryListbox(
-    frame,
-    width=25,
-    height=10,
-    font=('Calibri, 12'),
-    completevalues=lijst,
-)
-entry_1.grid(row=2, column=1)
+entry_1.bind('<KeyRelease>', check)
+update(lijst)
 
-entry_2 = AutocompleteEntryListbox(
-    frame,
-    width=25,
-    height=10,
-    font=('Calibri, 12'),
-    completevalues=lijst,
-    )
-entry_2.grid(row=2, column=2)
+#entry_2 = AutocompleteEntryListbox(
+#   frame,
+#   width=25,
+#   height=10,
+#   font=('Calibri, 12'),
+#   completevalues=lijst,
+#   )
+#entry_2.grid(row=2, column=2)
 
-entry_3 = AutocompleteEntryListbox(
-    frame,
-    width=25,
-    height=10,
-    font=('Calibri, 12'),
-    completevalues=lijst
-)
-entry_3.grid(row=2, column=3)
+#entry_3 = AutocompleteEntryListbox(
+#   frame,
+#   width=25,
+#   height=10,
+#   font=('Calibri, 12'),
+#   completevalues=lijst
+#)
+#entry_3.grid(row=2, column=3)
 
 
 #OPTIE 3: op basis van fotograaf
